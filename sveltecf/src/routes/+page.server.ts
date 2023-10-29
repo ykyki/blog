710340da882fabcf9a1d19af55b45c3360e0d462
ykyki <bsyki@pm.me>
2023-10-29 12:13:12 +0900

    sveletecf: add fs.readFile example

--- sveltecf/src/routes/+page.server.ts
+++ sveltecf/src/routes/+page.server.ts
@@ -1,9 +1,20 @@
 import type { PageServerLoad } from './$types';
+import { promises as fs } from 'fs';
 
 export const prerender = true;
 
 export const load = (async () => {
+    const fcont1 = await fs.readFile(
+        '../content/2021/0004-texdoc-default-viewer.md',
+        'utf-8',
+    );
+    const fcont2 = await fs.readFile(
+        '../content/2021/0001-brouwer-fixed-point-theorem-print.md',
+        'utf-8',
+    );
+
     return {
         date: new Date().toISOString(),
+        fcont: [fcont1, fcont2],
     };
 }) satisfies PageServerLoad;
--- sveltecf/src/routes/+page.svelte
+++ sveltecf/src/routes/+page.svelte
@@ -3,8 +3,16 @@
 
     export let data: PageData;
 
-    const { date } = data;
+    const { date, fcont } = data;
 </script>
 
 <h1>blog.ykyki.net</h1>
 <p>rendered at {date}</p>
+<div>
+    fcontent:
+    <ol>
+        {#each fcont as item}
+            <li>{item}</li>
+        {/each}
+    </ol>
+</div>
