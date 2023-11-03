b4b43e9211e9989da4726b9ba4a3701f202a1fd3
ykyki <bsyki@pm.me>
2023-11-04 00:48:56 +0900

    uniproc: add unified

--- uniproc/src/main.ts
+++ uniproc/src/main.ts
@@ -1,7 +1,21 @@
 import path from 'node:path';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkFrontmatter from 'remark-frontmatter';
+import rehypeStringify from 'rehype-stringify';
+import remarkRehype from 'remark-rehype';
 import { loadAllArticles } from 'loader';
+
+const processor = unified()
+    .use(remarkParse)
+    .use(remarkFrontmatter)
+    .use(remarkRehype)
+    .use(rehypeStringify);
 
 const contentPath = path.join(process.cwd(), '..', 'content');
 
 const articles = await loadAllArticles(contentPath);
-console.log(articles[0]);
+// console.log(articles[0]);
+
+const processorResult = await processor.process(articles[0].getSource());
+console.log(processorResult);
