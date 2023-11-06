b25c4a18426d26cf132c2fc4d76aea419008d6c2
ykyki <bsyki@pm.me>
2023-11-06 23:25:11 +0900

    WIP: add ArticleComponent

--- uniproc/src/Article.ts
+++ uniproc/src/Article.ts
@@ -3,9 +3,13 @@ import remarkParse from 'remark-parse';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
+import { is } from 'unist-util-is';
+import { visit } from 'unist-util-visit';
 import yaml from 'js-yaml';
 import { z } from 'zod';
 import { Root } from 'remark-parse/lib';
+import { Node } from 'mdast';
+
 export default class Article {
     readonly source: string;
 
@@ -24,14 +28,43 @@ export default class Article {
     get frontmatter(): Frontmatter | undefined {
         return extractFrontmatter(this.remarkTree);
     }
+
+    get body(): ArticleBody {
+        return extractArticleBody(this.remarkTree);
+    }
 }
 
-const remarkProcessor = () =>
-    unified()
-        .use(remarkParse)
-        .use(remarkFrontmatter, ['yaml'])
-        .use(remarkGfm, { singleTilde: false })
-        .use(remarkMath, { singleDollarTextMath: true });
+type ArticleBody = {
+    headings: Heading[];
+    components: ArticleComponent[];
+};
+
+type ArticleComponent = Heading | Paragraph | List | Unclassified;
+
+type Heading = {
+    type: 'heading';
+    depth: number;
+    value: string;
+};
+
+type Paragraph = {
+    type: 'paragraph';
+    value: string;
+};
+
+type List = {
+    type: 'list';
+    ordered: boolean;
+    items: ListItem[];
+};
+
+type ListItem = string;
+
+type Unclassified = {
+    type: 'unclassified';
+    typeValue: string;
+    value: string;
+};
 
 const frontmatterSchema = z.object({
     title: z.string(),
@@ -40,15 +73,106 @@ const frontmatterSchema = z.object({
 });
 type Frontmatter = z.infer<typeof frontmatterSchema>;
 
+const remarkProcessor = () =>
+    unified()
+        .use(remarkParse)
+        .use(remarkFrontmatter, ['yaml'])
+        .use(remarkGfm, { singleTilde: false })
+        .use(remarkMath, { singleDollarTextMath: true });
+
 const extractFrontmatter = (tree: Root): Frontmatter | undefined => {
     const node = tree.children[0];
-    if (node.type === 'yaml') {
+    if (is(node, 'yaml')) {
         const meta = frontmatterSchema.safeParse(yaml.load(node.value));
 
         if (meta.success) {
             return meta.data;
         }
     }
-
     return undefined;
+};
+
+const extractArticleBody = (tree: Root): ArticleBody => {
+    const components: ArticleComponent[] = [];
+
+    visit(
+        tree,
+        (node) => !is(node, 'listItem'),
+        (node) => {
+            let c: ArticleComponent;
+            switch (node.type) {
+                case 'heading': {
+                    if (is(node.children[0], 'text')) {
+                        const value = node.children[0].value;
+
+                        c = {
+                            type: 'heading',
+                            depth: node.depth,
+                            value: value,
+                        };
+                    } else {
+                        c = convertNodeToUnclassified(node);
+                    }
+                    break;
+                }
+                case 'list': {
+                    const ordered = node?.ordered ?? false;
+
+                    const items = [] as ListItem[];
+                    visit(node, 'listItem', (n) => {
+                        if (is(n.children[0], 'paragraph')) {
+                            if (is(n.children[0].children[0], 'text')) {
+                                items.push(n.children[0].children[0].value);
+                            }
+                        }
+                    });
+
+                    c = {
+                        type: 'list',
+                        ordered,
+                        items,
+                    };
+                    break;
+                }
+                case 'paragraph':
+                    if (is(node.children[0], 'text')) {
+                        const value = node.children[0].value;
+
+                        c = {
+                            type: 'paragraph',
+                            value: value,
+                        };
+                    } else {
+                        c = convertNodeToUnclassified(node);
+                    }
+                    break;
+                default:
+                    c = convertNodeToUnclassified(node);
+            }
+
+            components.push(c);
+        },
+    );
+
+    const headings = components.filter(
+        (c) => c.type === 'heading',
+    ) as Heading[];
+
+    return {
+        headings,
+        components,
+    };
+};
+
+const convertNodeToUnclassified = (node: Node): Unclassified => {
+    let value = '';
+    if ('value' in node && typeof node.value === 'string') {
+        value = node.value;
+    }
+
+    return {
+        type: 'unclassified',
+        typeValue: node.type,
+        value,
+    };
 };
--- uniproc/src/main.ts
+++ uniproc/src/main.ts
@@ -4,6 +4,7 @@ import remarkParse from 'remark-parse';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
+import { visit } from 'unist-util-visit';
 import { loadAllArticles } from './loader';
 
 const contentPath = path.join(process.cwd(), '..', 'content');
@@ -11,19 +12,33 @@ const contentPath = path.join(process.cwd(), '..', 'content');
 const articles = await loadAllArticles(contentPath);
 console.log(articles.map((article) => article.frontmatter));
 
+const article = articles[2];
+
 const tree = unified()
     .use(remarkParse)
     .use(remarkFrontmatter, ['yaml'])
     .use(remarkGfm, { singleTilde: false })
     .use(remarkMath, { singleDollarTextMath: true })
-    .parse(articles[3].source);
+    .parse(article.source);
 
 // console.log(tree);
 // console.log(tree.children);
-console.log(tree.children[0]);
+// console.log(tree.children[0]);
 // console.log(tree.children[1]);
 // console.log(tree.children[2]);
 // for (let i = 0; i < 7; i++) {
 //     console.log(tree.children[i]);
 // }
 // console.log(JSON.stringify(tree.children, null, 2));
+// visit(tree, ['list', 'listItem'], (node) => {
+//     console.log(node);
+// });
+
+console.log('------------------abc');
+
+// console.log(articles[2].body);
+article.body.components.forEach((c) => {
+    if (c.type === 'list') {
+        console.log(c);
+    }
+});
