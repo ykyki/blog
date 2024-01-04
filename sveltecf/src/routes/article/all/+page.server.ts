2ba43d81080062c01fb38f82302abc9993284607
ykyki <bsyki@pm.me>
2024-01-05 00:18:45 +0900

    sveltecf: create /article/all page for dev

--- sveltecf/src/routes/article/all/+page.server.ts
+++ sveltecf/src/routes/article/all/+page.server.ts
@@ -4,11 +4,9 @@ import type { PageServerLoad } from './$types';
 export const prerender = true;
 
 export const load = (async () => {
-    const articles = (await fetchAllArticles())
-        .filter((a) => a.frontmatter.draft === false)
-        .sort((a, b) =>
-            a.frontmatter.createdAt > b.frontmatter.createdAt ? 1 : -1,
-        );
+    const articles = (await fetchAllArticles()).sort((a, b) =>
+        a.frontmatter.createdAt > b.frontmatter.createdAt ? 1 : -1,
+    );
 
     return {
         date: new Date().toISOString(),
---
+++ sveltecf/src/routes/article/all/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Article from '$lib/component/Article.svelte';
+    import type { PageData } from './$types';
+
+    export let data: PageData;
+
+    const { articles } = data;
+</script>
+
+{#each articles as article}
+    {@const titleLink = `/article/${article.frontmatter.slug}`}
+    <Article {titleLink} {article} />
+    <hr />
+{/each}
