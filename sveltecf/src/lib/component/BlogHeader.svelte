7cce707119238cd75d3ab57a65c33f842665ab93
ykyki <bsyki@pm.me>
2024-01-22 00:33:51 +0900

    sveltecf: About page is created

--- sveltecf/src/lib/component/BlogHeader.svelte
+++ sveltecf/src/lib/component/BlogHeader.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
     import Logo from '$lib/component/Logo.svelte';
+    import { resolveRoute } from '$app/paths';
 </script>
 
 <div>
     <nav>
         <Logo size="large" />
+        <a href={resolveRoute('/about', {})}>About</a>
     </nav>
 </div>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: baseline;
+    }
+
+    nav a {
+        color: var(--color-on-primary);
+        text-decoration: none;
+    }
+
+    nav a:hover {
+        text-decoration: underline;
+    }
+</style>
---
+++ sveltecf/src/routes/about/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import BlogBase from '$lib/component/layout/BlogBase.svelte';
+    import Icon from '@iconify/svelte';
+</script>
+
+<BlogBase>
+    <article>
+        <h1>About</h1>
+        <p><b>🚧under construction🚧</b></p>
+        <h2>Links</h2>
+        <ol>
+            <li>
+                GitHub<Icon icon="mdi:github" />:
+                <a href="https://github.com/ykyki">https://github.com/ykyki</a>
+            </li>
+            <li>
+                Zenn<Icon icon="simple-icons:zenn" />:
+                <a href="https://zenn.dev/ykyki">https://zenn.dev/ykyki</a>
+            </li>
+            <li>
+                Bluesky<Icon icon="ri:bluesky-fill" />:
+                <a href="https://bsky.app/profile/ykyki.bsky.social"
+                    >https://bsky.app/profile/ykyki.bsky.social</a>
+            </li>
+        </ol>
+    </article>
+</BlogBase>
+
+<style>
+    :global(svg) {
+        display: inline;
+        font-size: var(--font-size-4);
+        margin: 0 var(--size-1);
+    }
+</style>
